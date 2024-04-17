ea886432d61556cbe5aa646564ed9a577530752d
Suprajeff <perin.vic@gmail.com>
2024-04-17 12:35:51 +0100

    Add GameRepository in go implementation

---
+++ back/go/app/core/repository/GameRepository.go
@@ -0,0 +1,12 @@
+package repository
+
+import "go-ttt/app/core/model"
+
+type GameRepository interface {
+	createNewGame() (*model.GameType, error)
+	resetGame() (*model.GameType, error)
+	updateBoard(row, col uint8, player *model.PlayerType) (*model.GameType, error)
+	switchCurrentPlayer() (*model.GameType, error)
+	getCurrentPlayer() (*model.PlayerType, error)
+	getBoardState() (*model.BoardType, error)
+}
