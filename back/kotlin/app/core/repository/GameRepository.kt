d89959186dac9967a3c1c9b98c56e7612a1a0535
Suprajeff <perin.vic@gmail.com>
2024-04-17 12:28:44 +0100

    Add GameRepository in kotlin implementation

---
+++ back/kotlin/app/core/repository/GameRepository.kt
@@ -0,0 +1,8 @@
+interface GameRepository {
+    fun createNewGame(): Result<GameType, Error>
+    fun resetGame(): Result<GameType, Error> 
+    fun updateBoard(row: Int, col: Int, player: PlayerType): Result<GameType, Error>
+    fun switchCurrentPlayer(): Result<GameType, Error> 
+    fun getCurrentPlayer(): Result<PlayerType, Error> 
+    fun getBoardState(): Result<BoardType, Error>
+}
