48b32ce981354dc63b74add9857c719d94bcb148
Suprajeff <perin.vic@gmail.com>
2024-04-17 12:21:25 +0100

    Add GameRepository in swift implementation

---
+++ back/swift/app/core/repository/GameRepository.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+protocol GameRepository {
+    func createNewGame() -> Result<GameType, Error>
+    func resetGame() -> Result<GameType, Error> 
+    func updateBoard(row: Int, col: Int, player: PlayerType) -> Result<GameType, Error>
+    func switchCurrentPlayer() -> Result<GameType, Error> 
+    func getCurrentPlayer() -> Result<PlayerType, Error> 
+    func getBoardState() -> Result<BoardType, Error>
+}
