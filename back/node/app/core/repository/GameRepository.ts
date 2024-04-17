1f7ed3d67587fc51f81660dc9d45d17823f257d5
Suprajeff <perin.vic@gmail.com>
2024-04-17 12:26:12 +0100

    Add GameRepository in node implementation

---
+++ back/node/app/core/repository/GameRepository.ts
@@ -0,0 +1,12 @@
+import {PlayerType} from "../model/PlayerType";
+import {BoardType} from "../model/BoardType";
+import {GameType} from "../model/GameType";
+
+interface GameRepository {
+    createNewGame(): Promise<Result<GameType>>;
+    resetGame(): Promise<Result<GameType>>;
+    updateBoard(row: number, col: number, player: PlayerType): Promise<Result<GameType>>;
+    switchCurrentPlayer(): Promise<Result<GameType>>;
+    getCurrentPlayer(): Promise<Result<PlayerType>>;
+    getBoardState(): Promise<Result<BoardType>>;
+}
