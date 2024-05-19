9bbe5f7d3095ea3c481b7e79be820b52f805df17
Suprajeff <perin.vic@gmail.com>
2024-05-19 12:38:53 +0100

    Update Package.swift (swift-tools-version to 5.10 + NIO imports and versioning)

--- back/swift/Package.swift
+++ back/swift/Package.swift
@@ -1,34 +1,36 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "swift-ttt",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     products: [
         .executable(name: "swift-ttt", targets: ["TicTacToe"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.42.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/redis.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
+        .package(url: "https://github.com/vapor/redis.git", from: "4.10.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.15.0")
     ],
     targets: [
         .executableTarget(
             name: "TicTacToe",
             dependencies: [
-                .product(name: "NIO", package: "swift-nio"),
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
                 .product(name: "Redis", package: "redis"),
-                .product(name: "WebSocketKit", package: "websocket-kit")
+                .product(name: "WebSocketKit", package: "websocket-kit"),
             ]
         ),
         .testTarget(
             name: "TicTacToeTests",
             dependencies: [
-                .target(name: "TicTacToe")
+                .target(name: "TicTacToe"),
+                .product(name: "XCTVapor", package: "vapor"),
             ]
         )
     ]
